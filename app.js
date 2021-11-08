931025a774fdac11b692e4fc65f11c40e71d961a
ahmed <ahmedlearn01@gmail.com>
2021-11-07 21:58:25 -0800

    initial commit

--- app.js
+++ app.js
@@ -7,8 +7,30 @@
 // fs.writeFileSync('notes.txt','this file was created by node js !');
 // fs.appendFileSync('notes.txt','this text appended from appendFileSync.');
 
+
+
+
+
+
+
+
+
+
+const validator = require('validator');
 const name = require('./utils');
 const getNotes = require('./notes')
 
 name('hey node js world here i am :)');
-console.log(getNotes());
+
+
+console.log(validator.isURL('HTTP://ahmed@gmail.com'));
+
+
+
+
+
+
+
+
+
+
